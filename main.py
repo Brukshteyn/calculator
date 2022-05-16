09d7cc0b3a6f5ecc572c552ee8de2f9c159fc777
Brukshteyn <r.brukshteyn@gmail.com>
2022-05-16 21:48:10 +0300

    Fix issue 3 Removed extra empty lines

--- main.py
+++ main.py
@@ -4,24 +4,21 @@ import datetime as dt
 class Calculator:
 
     def __init__(self, limit):
-
         self.limit = limit
         self.records = []
 
     def add_record(self, record):
-
         self.records.append(record)
 
     def get_today_stats(self):
-
         return sum([rec.amount for rec in self.records if rec.date == dt.datetime.now().date()])
 
     def get_week_stats(self):
-
         return sum([rec.amount for rec in self.records if rec.date > dt.datetime.now().date() - dt.timedelta.days(7)])
 
 
 class CaloriesCalculator(Calculator):
+
 
     def get_calories_remained(self):
         current = self.get_today_stats()
@@ -32,16 +29,16 @@ class CaloriesCalculator(Calculator):
 
 
 class CashCalculator(Calculator):
+
+
     USD_RATE = 63.3
     EURO_RATE = 65.7
 
     def get_today_cash_remained(self, currency):
-
         current = self.get_today_stats()
         dic_currency = {'rub': [1, 'руб'],
                         'usd': [self.USD_RATE, 'USD'],
                         'eur': [self.EURO_RATE, 'Euro']}
-
         if current >= self.limit:
             return f'Денег нет, держись'
         else:
@@ -50,6 +47,7 @@ class CashCalculator(Calculator):
 
 
 class Record:
+
 
     def __init__(self, amount, comment, date=None):
         self.amount = amount
@@ -66,7 +64,6 @@ class Record:
 if __name__ == '__main__':
     # для CashCalculator
     r1 = Record(amount=3000, comment='Кофе')
-
     cash_calculator = CashCalculator(1000)
     cash_calculator.add_record(Record(amount=145, comment='кофе'))
     cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
@@ -74,12 +71,10 @@ if __name__ == '__main__':
                                       comment='бар в Танин др',
                                       date='08.11.2019'))
     print(cash_calculator.get_today_cash_remained('rub'))
-
     # для CaloriesCalculator
     r2 = Record(amount=1186,
                 comment='Кусок тортика. И ещё один.',
                 date='24.02.2019')
-
     cal_calculator = CaloriesCalculator(400)
     cal_calculator.add_record(Record(amount=145, comment='кофе'))
     cal_calculator.add_record(Record(amount=300, comment='обед'))
